d103005ec8e1531a6353ad82b37efb37264e3bd9
CallMeQuy <104016339+CallMeQuy@users.noreply.github.com>
2024-04-03 17:47:56 +0700

    Finish

--- NongHaiQuy.py
+++ NongHaiQuy.py
@@ -1,2 +1,30 @@
-#Nông Hải Quý
-#Đề bài: Đọc 1 file văn bản gồm nhiều dòng. GHi ra màn hình theo thứ tự ngược lại của các dòng
+# Nông Hải Quý
+# Đề bài: Đọc 1 file văn bản gồm nhiều dòng. GHi ra màn hình theo thứ tự ngược lại của các dòng
+# Bài làm
+# Kiểm tra đuôi tệp
+def check_file_extension(filename):
+    allowed_extensions = ['.txt']
+
+    if not any(filename.endswith(ext) for ext in allowed_extensions):
+        return False
+    return True
+
+# Nhập tên tệp từ người dùng
+filename = input("Nhập tên tệp: ")
+
+# Kiểm tra đuôi tệp
+if not check_file_extension(filename):
+    print("Lỗi file: Đuôi tệp không hợp lệ.")
+else:
+    try:
+        # Mở file văn bản để đọc
+        with open(filename, 'r', encoding='utf-8'Bai) as file:
+            # Đọc từng dòng trong file và lưu vào list
+            lines = file.readlines()
+
+            # In các dòng theo thứ tự ngược lại
+            for line in reversed(lines):
+                print(line.strip())
+
+    except FileNotFoundError:
+        print("Không tồn tại tệp.")
